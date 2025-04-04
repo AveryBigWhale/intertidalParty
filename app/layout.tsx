c0f11d95e47f1803efa19bf54bb99f56fb9fa36e
AveryBigWhale <averybigwhale@gmail.com>
2025-04-04 12:51:01 +0800

    modified package json for mailing func try .js

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <footer className="bg-gray-800 text-intertidalLightMain py-6 text-center mt-10">
             <div className="container mx-auto">
               <h3 className="text-lg font-semibold">潮間黨 Intertidal Party</h3>
-              <p>地址 | 105 台北市松山區南京東路三段261號3樓</p>
-              <p>電話 | 02-27520806</p>
-              <p>信箱 | chairperson@dpp.org.tw</p>
+              {/* <p>地址 | 105 台北市松山區南京東路三段261號3樓</p> */}
+              {/* <p>電話 | 02-27520806</p> */}
+              {/* <p>信箱 | chairperson@dpp.org.tw</p> */}
             </div>
           </footer>
         </body>
